49ee4c03edbcf5a4eb6d0dd24ded1ac9f4c0a582
bruna_superfrete <bruna.andrade@superfrete.com>
2022-03-05 16:24:25 -0300

    Exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,15 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo";
+
+//Concatenação (+)
+
+console.log(citacao + meuNome)
+
+
+
+//Template string ou template literal
